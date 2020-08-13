660b222efb223cf8756144f8f55fd00f851987be
gaoussoukebe <33683408+gaoussoukebe@users.noreply.github.com>
2020-08-13 19:37:58 -0400

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -65,12 +65,10 @@ def evaluate(experiment_name, test_data_path, pos_neg_examples_file, gpu_num, em
 
     language_model = RowNet(language_dim, embed_dim=embedded_dim)
     vision_model = RowNet(vision_dim, embed_dim=embedded_dim)
-    language_model.load_state_dict(torch.load(os.path.join(train_results_dir, 'model_A_state.pt')))
-    vision_model.load_state_dict(torch.load(os.path.join(train_results_dir, 'model_B_state.pt')))
-    print(device)
-    if not torch.cuda.is_available():
-        language_model.load_state_dict(torch.load(os.path.join(train_results_dir, 'model_A_state.pt'), map_location=device))
-        vision_model.load_state_dict(torch.load(os.path.join(train_results_dir, 'model_B_state.pt'), map_location=device))
+    # language_model.load_state_dict(torch.load(os.path.join(train_results_dir, 'model_A_state.pt')))
+    # vision_model.load_state_dict(torch.load(os.path.join(train_results_dir, 'model_B_state.pt')))
+    language_model.load_state_dict(torch.load(os.path.join(train_results_dir, 'model_A_state.pt'), map_location=device))
+    vision_model.load_state_dict(torch.load(os.path.join(train_results_dir, 'model_B_state.pt'), map_location=device))
     language_model.to(device)
     vision_model.to(device)
     language_model.eval()
