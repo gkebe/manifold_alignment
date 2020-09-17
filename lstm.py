4354f9fd4ae6b63dd3dbaadfe9a1d320832a0bcf
gaoussoukebe <33683408+gaoussoukebe@users.noreply.github.com>
2020-09-17 15:43:50 -0400

    Update lstm.py

--- lstm.py
+++ lstm.py
@@ -2,69 +2,35 @@ import torch
 import torch.nn.functional as F
 
 class LSTM(torch.nn.Module):
-    def __init__(self, input_size, output_size, hidden_dim, num_layers, dropout, device, bilstm= False, more_fc=False):
+    def __init__(self, input_size, output_size, hidden_dim, num_layers, mean_pooling, device):
         super(LSTM, self).__init__()
         self.device = device
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
-        self.bilstm = bilstm
-        self.more_fc = more_fc
-        if bilstm:
-            self.lstm = torch.nn.LSTM(input_size, hidden_dim/2, num_layers,
-                                      batch_first=True, dropout=dropout, bidirectional=True)
-        else:
-            self.lstm = torch.nn.LSTM(input_size, hidden_dim, num_layers,
-                batch_first=True, dropout=dropout)
-        if more_fc:
-            self.fc1 = torch.nn.Linear(hidden_dim, hidden_dim)
-            self.fc2 = torch.nn.Linear(hidden_dim, hidden_dim)
-            self.fc3 = torch.nn.Linear(hidden_dim, output_size)
-        else:
-            self.fc = torch.nn.Linear(hidden_dim, output_size)
+        self.lstm = torch.nn.LSTM(input_size, hidden_dim/2, num_layers,
+                                      batch_first=True, bidirectional=True)
+        self.fc1 = torch.nn.Linear(hidden_dim, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = torch.nn.Linear(hidden_dim, output_size)
+        self.hidden = self.init_hidden()
+        self.mean_pooling = mean_pooling
+    def init_hidden(self):
+        return (torch.randn(2, 1, self.hidden_dim // 2, device=self.device),
+                torch.randn(2, 1, self.hidden_dim // 2, device=self.device))
 
     def forward(self, X):
-        batch_size = X.size(0)
-        h_t = torch.zeros(self.num_layers, batch_size,
-            self.hidden_dim).to(self.device)
-        c_t = torch.zeros(self.num_layers, batch_size,
-            self.hidden_dim).to(self.device)
+        self.hidden = self.init_hidden()
+        steps = X.size()[0]
+        lstm_out, self.hidden = self.lstm(X, self.hidden)
+        lstm_out = lstm_out.view(steps, self.hidden_dim)
 
-        # if self.k is not None:
-        #     T = X.size(1)
-        #     for t, x in enumerate(X.split(1, 1)):
-        #         #print(x)
-        #         #print(X.size())
-        #         #print(x.size())
-        #
-        #         out, (h_t, c_t) = self.lstm(x, (h_t, c_t))
-        #         # perform TBPTT if set
-        #         if T - t == self.k:
-        #             out.detach()
-        # else:
-        #     out, (h_t, c_t) = self.lstm(X, (h_t, c_t))
-        #     seq_len = out.size(1)
-        #     #print(f'init size: {out.size()}')
-        #     out = out.narrow(
-        #         1,
-        #         max(0, seq_len - self.awe),
-        #         min(self.awe, seq_len)
-        #     ).flatten(1)
-        #     #print(f'out  size: {out.size()}')
-        out, (h_t, c_t) = self.lstm(X, (h_t, c_t))
-        if self.bilstm:
-            out = h_t.view(self.num_layers, batch_size, self.hidden_dim)[-1]
+        if self.mean_pooling:
+            out = torch.mean(lstm_out)
         else:
-            out = h_t.view(self.num_layers, batch_size, self.hidden_dim)[-1]
-        #print(f'hidden size: {hidden.size()}')
-        # This does some reshaping, might be an old idiom
-        # see: https://discuss.pytorch.org/t/when-and-why-do-we-use-contiguous/47588
-        #hidden = self.contiguous().view(-1, self.hidden_dim)
-        #hidden = self.fc(hidden)
-        if self.more_fc:
-            out = F.leaky_relu(self.fc1(out), negative_slope=.2)
-            out = F.leaky_relu(self.fc2(out), negative_slope=.2)
-            out = self.fc3(out)
-        else:
-            out = self.fc(out)
+            out = lstm_out[-1]
+
+        out = F.leaky_relu(self.fc1(out), negative_slope=.2)
+        out = F.leaky_relu(self.fc2(out), negative_slope=.2)
+        out = self.fc3(out)
 
         return out
