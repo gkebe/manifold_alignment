6cc6e4c9d54b8fe53132bba03d9c8ab30b1083c8
gaoussoukebe <33683408+gaoussoukebe@users.noreply.github.com>
2020-09-29 00:30:45 -0400

    Update rownet.py

--- rownet.py
+++ rownet.py
@@ -1,39 +1,6 @@
 import torch
 import torch.nn.functional as F
-def cov(m, rowvar=True, inplace=False):
-    '''Estimate a covariance matrix given data.
 
-    Covariance indicates the level to which two variables vary together.
-    If we examine N-dimensional samples, `X = [x_1, x_2, ... x_N]^T`,
-    then the covariance matrix element `C_{ij}` is the covariance of
-    `x_i` and `x_j`. The element `C_{ii}` is the variance of `x_i`.
-
-    Args:
-        m: A 1-D or 2-D array containing multiple variables and observations.
-            Each row of `m` represents a variable, and each column a single
-            observation of all those variables.
-        rowvar: If `rowvar` is True, then each row represents a
-            variable, with observations in the columns. Otherwise, the
-            relationship is transposed: each column represents a variable,
-            while the rows contain observations.
-
-    Returns:
-        The covariance matrix of the variables.
-    '''
-    if m.dim() > 2:
-        raise ValueError('m has more than 2 dimensions')
-    if m.dim() < 2:
-        m = m.view(1, -1)
-    if not rowvar and m.size(0) != 1:
-        m = m.t()
-    # m = m.type(torch.double)  # uncomment this line if desired
-    fact = 1.0 / (m.size(1) - 1)
-    if inplace:
-        m -= torch.mean(m, dim=1, keepdim=True)
-    else:
-        m = m - torch.mean(m, dim=1, keepdim=True)
-    mt = m.t()  # if complex: mt = m.t().conj()
-    return fact * m.matmul(mt).squeeze()
 class RowNet(torch.nn.Module):
     def __init__(self, input_size, embedded_dim=1024):
         # Language (BERT): 3072, Vision + Depth (ResNet152): 2048 * 2
@@ -41,12 +8,10 @@ class RowNet(torch.nn.Module):
         self.fc1 = torch.nn.Linear(input_size, input_size)
         self.fc2 = torch.nn.Linear(input_size, input_size)
         self.fc3 = torch.nn.Linear(input_size, embedded_dim)
-        self.dropout1 = torch.nn.Dropout(p=0.5)
-        self.dropout2 = torch.nn.Dropout(p=0.2)
 
     def forward(self, x):
-        x = F.leaky_relu(self.dropout1(self.fc1(x)), negative_slope=.2)
-
-        x = F.leaky_relu(self.dropout2(self.fc2(x)), negative_slope=.2)
+        x = F.leaky_relu(self.fc1(x), negative_slope=.2)
+        x = F.leaky_relu(self.fc2(x), negative_slope=.2)
         x = self.fc3(x)
+
         return x
