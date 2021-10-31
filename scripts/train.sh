2eb1ba676d4591c7fc1914c2d32690a79a7d0671
gkebe <56805919+gkebe@users.noreply.github.com>
2021-10-31 19:06:43 -0400

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,7 +2,11 @@
 cd "$(dirname "$0")"
 cd ..
 model=${1}
-run=${2}
 epochs=${3}
-seed=${4}
-python train.py --experiment_name=${model}_${run} --epochs=${epochs} --seed=${seed} --train_data=data/gld_${model}_vision_train.pkl --pos_neg_examples_file=data/gld_${model}_vision_train_pos_neg.pkl --dev_data=data/gld_${model}_vision_dev.pkl --test_data=data/gld_${model}_vision_test.pkl --gpu_num=0
+initial_seed=${4}
+gpu=${5}
+for i in {1..${2}}
+do
+   seed=$((initial_seed + i))
+   python train.py --experiment_name=${model}_${i} --epochs=${epochs} --seed=${seed} --train_data=data/gld_${model}_vision_train.pkl --pos_neg_examples_file=data/gld_${model}_vision_train_pos_neg.pkl --dev_data=data/gld_${model}_vision_dev.pkl --test_data=data/gld_${model}_vision_test.pkl --gpu_num=${gpu}
+done
