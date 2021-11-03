425892337bd9bd76a355978f39b29438e3fc5d97
gkebe <56805919+gkebe@users.noreply.github.com>
2021-11-03 04:12:12 -0400

    Update epoch_plots.sh

--- scripts/epoch_plots.sh
+++ scripts/epoch_plots.sh
@@ -4,5 +4,5 @@ cd ..
 for i in $(seq 1 5); 
 do 
 python plotting/create_epochs_plots.py --experiment ${1}_${i} --threshold ${2} --dev
-#mv output/${1}_${i}/${1}_${i}_epochs.pkl plots/ 
+mv output/${1}_${i}/${1}_${i}_epochs.pkl plots/
 done
