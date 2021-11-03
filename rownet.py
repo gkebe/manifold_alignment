b991ffb77e3223e7857bc570b3b4783dfe00d82b
gkebe <56805919+gkebe@users.noreply.github.com>
2021-11-03 03:46:40 -0400

    fixed _dev

--- rownet.py
+++ rownet.py
@@ -6,12 +6,10 @@ class RowNet(torch.nn.Module):
         # Language (BERT): 3072, Vision + Depth (ResNet152): 2048 * 2
         super(RowNet, self).__init__()
         self.fc1 = torch.nn.Linear(input_size, input_size)
-        print(input_size)
         self.fc2 = torch.nn.Linear(input_size, input_size)
         self.fc3 = torch.nn.Linear(input_size, embedded_dim)
 
     def forward(self, x):
-        print(x.shape)
         x = F.leaky_relu(self.fc1(x), negative_slope=.2)
         x = F.leaky_relu(self.fc2(x), negative_slope=.2)
         x = self.fc3(x)
--- scripts/f1_plots.sh
+++ scripts/f1_plots.sh
@@ -3,6 +3,6 @@ cd "$(dirname "$0")"
 cd ..
 for i in $(seq 1 5); 
 do 
-python plotting/create_f1_plots_dev.py --experiment ${1}_${i} --epoch ${2} --dev
+python plotting/create_f1_plots.py --experiment ${1}_${i} --epoch ${2} --dev
 mv output/${1}_${i}/${1}_${i}_p_r_f1.pkl plots/ 
 done
