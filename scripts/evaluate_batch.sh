463659216fd15c52afd8df914918cf4cd14e6145
gkebe <56805919+gkebe@users.noreply.github.com>
2021-11-03 04:44:45 -0400

    Update evaluate_batch.sh

--- scripts/evaluate_batch.sh
+++ scripts/evaluate_batch.sh
@@ -4,6 +4,6 @@ cd ..
 for i in $(seq 1 5); 
 do
 echo ${1}_${i}
-python evaluate.py --experiment_name ${1}_${i}_bias --test_data_path ./data/gld_${1}_vision_test.pkl --pos_neg_examples_file ./data/gld_${1}_vision_test_pos_neg.pkl --threshold ${2} --epoch ${3}
+python evaluate.py --experiment_name ${1}_${i} --test_data_path ./data/gld_${1}_vision_test.pkl --pos_neg_examples_file ./data/gld_${1}_vision_test_pos_neg.pkl --threshold ${2} --epoch ${3}
 #mv output/${1}_${i}/${1}_${i}_epochs.pkl plots/
 done
