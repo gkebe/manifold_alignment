0db6d2ac11b9eea0a17aedfeb8dd9c2be61e2c2d
gkebe <56805919+gkebe@users.noreply.github.com>
2022-04-03 18:38:00 -0400

    added speech_featurization speech_transcription

---
+++ gold_featurization/speech_representations/speech_featurization.py
@@ -0,0 +1,47 @@
+import argparse
+from collections import defaultdict
+import os
+import pickle
+
+import torch
+import torchaudio
+from tqdm import tqdm
+from transformers import Wav2Vec2Processor, Wav2Vec2Model
+
+import argparse
+import soundfile as sf
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--speech_dir', help='speech directory', required=True)
+    parser.add_argument('--model', help='pretrained model', required=True)
+    parser.add_argument('--output', help='output file name (.pkl file with embeddings)', required=True)
+
+    return parser.parse_known_args()
+
+ARGS, unused = parse_args()
+if ARGS.model == "wav2vec2":
+    processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
+    model = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base-960h").cuda()
+
+elif ARGS.model == "wavlm":
+    processor = Wav2Vec2Processor.from_pretrained("patrickvonplaten/wavlm-libri-clean-100h-base-plus")
+    model = WavLMModel.from_pretrained("patrickvonplaten/wavlm-libri-clean-100h-base-plus").cuda()
+
+model.eval()
+speech_dict = dict()
+data_path = ARGS.speech_dir
+
+
+for file_name in tqdm(os.listdir(data_path)):
+    wav_path = os.path.join(data_path, file_name)
+    speech, _ = sf.read(wav_path)
+    input_values = processor(speech, return_tensors="pt", sampling_rate=16000).input_values.cuda()
+    with torch.no_grad():
+       hidden_states = model(input_values, output_hidden_states=True).hidden_states
+    hidden_states = torch.cat([i for i in hidden_states][-4:]).transpose(0,1).contiguous().view(-1, 3072)
+    features = torch.mean(hidden_states, dim=0).view(-1)
+    speech_dict[file_name.replace(".wav", "")] = features.detach().cpu()
+
+with open(ARGS.output, 'wb') as f:
+    pickle.dump(speech_dict, f)
---
+++ gold_featurization/speech_representations/speech_transcription.py
@@ -0,0 +1,51 @@
+import argparse
+from collections import defaultdict
+import os
+import pickle
+
+import torch
+import torchaudio
+from tqdm import tqdm
+from transformers import Wav2Vec2Tokenizer, Wav2Vec2ForCTC
+
+import soundfile as sf
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--speech_dir', help='speech directory', required=True)
+    parser.add_argument('--model', help='pretrained model', required=True)
+    parser.add_argument('--output', help='output file name (.pkl file with transcriptions)', required=True)
+
+    return parser.parse_known_args()
+
+ARGS, unused = parse_args()
+
+if ARGS.model == "wav2vec2":
+    processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
+    model = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base-960h").cuda()
+
+elif ARGS.model == "wavlm":
+    processor = Wav2Vec2Processor.from_pretrained("patrickvonplaten/wavlm-libri-clean-100h-base-plus")
+    model = WavLMModel.from_pretrained("patrickvonplaten/wavlm-libri-clean-100h-base-plus").cuda()
+
+model.eval()
+data_path = ARGS.speech_dir
+speech_dict = dict()
+
+for file_name in tqdm(os.listdir(data_path)):
+    wav_path = os.path.join(data_path, file_name)
+    speech, _ = sf.read(wav_path)
+    input_values = tokenizer(speech, return_tensors="pt", padding="longest",
+                             sampling_rate=16000).input_values  # Batch size 1
+
+    # retrieve logits
+    with torch.no_grad():
+        logits = model(input_values.cuda()).logits.detach().cpu()
+
+    # take argmax and decode
+    predicted_ids = torch.argmax(logits, dim=-1)
+    transcription = tokenizer.batch_decode(predicted_ids)
+    speech_dict[file_name.replace(".wav", "")] = transcription
+
+with open(ARGS.output, 'wb') as f:
+    pickle.dump(speech_dict, f)
