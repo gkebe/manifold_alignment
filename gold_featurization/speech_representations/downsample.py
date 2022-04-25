fbbd4732d977fcf79d04ab5ad0727321f1fae0b0
gkebe <56805919+gkebe@users.noreply.github.com>
2022-04-25 12:16:13 -0400

    Create downsample.py

---
+++ gold_featurization/speech_representations/downsample.py
@@ -0,0 +1,24 @@
+import librosa
+import soundfile as sf
+import os
+from tqdm import tqdm
+sr = 16000
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--speech_dir', help='speech directory', required=True)
+    parser.add_argument('--output_dir', help='output directory', required=True)
+
+    return parser.parse_known_args()
+
+ARGS, unused = parse_args()
+
+directory = args.speech_dir
+output_dir = args.output_dir
+
+for filename in tqdm(os.listdir(directory)):
+    #print(os.path.join(directory, filename))
+    y, s = librosa.load(os.path.join(directory, filename), sr=sr)
+#    print(s)
+#    print(sr)
+    sf.write(os.path.join(output_dir, filename), y, s)
